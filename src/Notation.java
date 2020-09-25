286768a8e802e13d2854f8cd0ada02eebcdbc432
Gabriel Figueiredo <gmfiga@gmail.com>
2020-09-25 13:28:30 -0400

    version 5 - convertPostfixToInfix implemented, working and passing all tests.evaluatePostfixExpression is implemented but does not pass tests.

--- src/Notation.java
+++ src/Notation.java
@@ -47,23 +47,21 @@ public class Notation {
 
             //if operators is found
             else if (character == '+' || character == '-' || character == '*' || character == '/') {
-                if (character == '+' || character == '-'){
+                if (character == '+' || character == '-') {
                     try {
                         while (stack.top() != '(') {
                             queue.enqueue(stack.pop());
                         }
-                    } catch (StackUnderflowException exception){
+                    } catch (StackUnderflowException exception) {
                     } finally {
                         stack.push(character);
                     }
-                }
-
-                else if ( character == '*' || character == '/'){
+                } else if (character == '*' || character == '/') {
                     try {
                         while (stack.top() == '*' || stack.top() == '/') {
                             queue.enqueue(stack.pop());
                         }
-                    } catch (StackUnderflowException exception){
+                    } catch (StackUnderflowException exception) {
                     } finally {
                         stack.push(character);
                     }
@@ -71,13 +69,13 @@ public class Notation {
             }
 
             //if right parenthesis is found
-            else if (character == ')'){
+            else if (character == ')') {
                 try {
-                    while (stack.top() != '('){
+                    while (stack.top() != '(') {
                         queue.enqueue(stack.pop());
                     }
                     stack.pop();
-                } catch (StackUnderflowException exception){
+                } catch (StackUnderflowException exception) {
                     throw new InvalidNotationFormatException();
                 }
 
@@ -97,8 +95,42 @@ public class Notation {
      */
 
     public static String convertPostfixToInfix(String postfix) throws InvalidNotationFormatException {
-        return "";
+
+        NotationStack<String> stack = new NotationStack<>();
+        String infix = "";
+
+        for (int i = 0; i < postfix.length(); i++) {
+            Character character = postfix.charAt(i);
+            //if operand found
+            if (character == '1' || character == '2' || character == '3' ||
+                    character == '4' || character == '5' || character == '6' ||
+                    character == '7' || character == '8' || character == '9') {
+
+                stack.push(character.toString());
+            }
+
+            //if operators is found
+            else if (character == '+' || character == '-' || character == '*' || character == '/') {
+                if (stack.size() < 2)
+                    throw new InvalidNotationFormatException();
+                else {
+                    String rightOperand = stack.pop();
+                    String leftOperand = stack.pop();
+                    String operation = "(" + leftOperand + character + rightOperand + ")";
+
+                    stack.push(operation);
+                }
+            }
+        }
+
+        if (stack.size() == 1)
+            infix = stack.toString();
+        else
+            throw new InvalidNotationFormatException();
+
+        return infix;
     }
+
 
     /**
      * Evaluates a postfix expression from a string to a double
@@ -107,7 +139,61 @@ public class Notation {
      * @return the evaluation of the postfix expression as a double
      * @throws InvalidNotationFormatException - if the postfix expression format is invalid
      */
-    public static double evaluatePostfixExpression(String postfixExpr) throws InvalidNotationFormatException {
-        return 0;
+    public static double evaluatePostfixExpression(String postfixExpr) {
+
+        NotationStack<Character> stack = new NotationStack<>();
+
+        double result = 0;
+        char[] charArray = postfixExpr.toCharArray();
+
+        for (int i = 0; i < postfixExpr.length(); i++) {
+
+            Character character = postfixExpr.charAt(i);
+
+            //if operand or left parenthesis found
+            if (character == '1' || character == '2' || character == '3' ||
+                    character == '4' || character == '5' || character == '6' ||
+                    character == '7' || character == '8' || character == '9' ||
+                    character == '(') {
+
+                stack.push(character);
+            }
+
+
+            //if operator is found
+            else if (character == '+' || character == '-' || character == '*' || character == '/') {
+                if (stack.size() < 2)
+                    throw new InvalidNotationFormatException();
+                else {
+                    double rightOperand = Character.getNumericValue(stack.pop());
+                    double leftOperand = Character.getNumericValue(stack.pop());
+
+                    if (character == '+') {
+                        Double operationResult = leftOperand + rightOperand;
+                        stack.push(operationResult.toString());
+                    } else if (character == '-') {
+                        Double operationResult = leftOperand - rightOperand;
+                        stack.push(operationResult.toString());
+                    } else if (character == '*') {
+                        Double operationResult = leftOperand * rightOperand;
+                        stack.push(operationResult.toString());
+                    } else if (character == '/') {
+                        Double operationResult = leftOperand / rightOperand;
+                        stack.push(operationResult.toString());
+                    }
+
+                }
+            }
+        }
+
+
+        if (stack.size() == 1) {
+            result = Character.getNumericValue(stack.pop());
+        } else {
+            throw new InvalidNotationFormatException();
+        }
+
+        return result;
     }
+
 }
